32dd8815a13b77224336a60d6e91fca0a1ff39b2
Mitchell Lisle <lislemitchell@gmail.com>
2022-01-04 19:28:31 +1100

    add initial working code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# noname
-An experimental data anonymisation library
+##  noname
+
+> 1️⃣ version: 0.1.0
+
+> ✍️ author: Mitchell Lisle
+
+📛 An experimental data anonymisation library
---
+++ src/noname/__init__.py
@@ -0,0 +1,17 @@
+__version__ = '0.1.0'
+__author__ = 'Mitchell Lisle'
+__email__ = 'm.lisle90@gmail.com'
+
+
+from noname.anonymiser import Anonymiser
+from noname.info_types import (
+    LongDigit,
+    InfoType,
+    Email,
+    AusPostCode,
+    AusDriversLicence,
+    AusPassport,
+    AusTaxFileNumber,
+    AusLicensePlate,
+    AusInfoTypes
+)
